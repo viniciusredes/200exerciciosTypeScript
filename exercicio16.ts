b209434a1f5eb0d9a730e3c0d52cb115105df440
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-10-02 22:26:55 -0300

    Escreva um programa que determina o maior entre três números.

---
+++ exercicio16.ts
@@ -0,0 +1,6 @@
+//Escreva um programa que determina o maior entre três números.
+let num1: number = 10;
+let num2: number = 20;
+let num3: number = 30;
+
+console.log(`${Math.max(num1,num2,num3)} é o maior número`);
