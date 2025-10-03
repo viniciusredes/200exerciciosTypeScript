10e78a6c59a22bcbfb3d3cacd0362fdcdda50293
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-10-03 20:16:57 -0300

    Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.

---
+++ exercicio18.ts
@@ -0,0 +1,7 @@
+//Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
+let idade: number = 18;
+if(idade >= 18){
+  console.log("Pode dirigir")
+}else{
+  console.log("Não pode dirigir")
+}
